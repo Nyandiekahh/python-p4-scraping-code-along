4452e68f574b0e2c3fec0f7aea2745eb3d50d984
Nyandiekah <einsteinmokua100@gmail.com>
2024-09-01 06:06:06 +0300

    Pass tests

--- lib/Course.py
+++ lib/Course.py
@@ -1,2 +1,14 @@
 class Course:
-    pass
+    def __init__(self, title=None, schedule=None, description=None):
+        self.title = title
+        self.schedule = schedule
+        self.description = description
+
+    def get_title(self):
+        return self.title
+
+    def get_schedule(self):
+        return self.schedule
+
+    def get_description(self):
+        return self.description
